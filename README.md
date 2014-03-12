80053a09bf7ea3e5b2884a51441fde1bc2edb367
Jamie McDonald <jdamcd@gmail.com>
2014-03-12 12:33:18 +0100

    Added empty example project

---
+++ README.md
@@ -0,0 +1,3 @@
+> I guess people are just cropping out all the sadness
+
+This is an Android library project to provide a simple image cropping `Activity`.
---
+++ example/src/main/java/com/soundcloud/android/crop/example/MainActivity.java
@@ -0,0 +1,16 @@
+package com.soundcloud.android.crop.example;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
