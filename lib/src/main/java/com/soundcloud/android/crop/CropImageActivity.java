140bb030f2cf49717878028ac97dff7088ba5666
Jamie McDonald <jdamcd@gmail.com>
2014-06-13 15:41:25 +0200

    Formatting [skip ci]

--- lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
+++ lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
@@ -18,7 +18,13 @@ package com.soundcloud.android.crop;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.graphics.*;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.net.Uri;
 import android.opengl.GLES10;
 import android.os.Build;
