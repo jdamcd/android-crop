a02be19f3fe6693a48ff5362d39ed4004ed97900
Brandon <brandon.haley94@gmail.com>
2014-11-07 21:39:02 -0800

    fixed dependency for android

--- lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
+++ lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
@@ -23,6 +23,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.net.Uri;
