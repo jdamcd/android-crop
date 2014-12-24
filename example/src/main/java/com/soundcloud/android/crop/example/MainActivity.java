8daaec1f00bf615d7f92545946368b8b7dfe7e43
Mateusz Klimek <mklimus@gmail.com>
2014-12-24 12:26:55 +0100

    revert example to version from master

--- example/src/main/java/com/soundcloud/android/crop/example/MainActivity.java
+++ example/src/main/java/com/soundcloud/android/crop/example/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends Activity {
 
     private void beginCrop(Uri source) {
         Uri outputUri = Uri.fromFile(new File(getCacheDir(), "cropped"));
-        new Crop(source).output(outputUri).withFixedSize(640, 640).start(this);
+        new Crop(source).output(outputUri).asSquare().start(this);
     }
 
     private void handleCrop(int resultCode, Intent result) {
